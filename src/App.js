000424a1a4709c64ffed51012b67f8d0ae3f5539
AnatoliBahdankevich <141944096+AnatoliBahdankevich@users.noreply.github.com>
2023-09-16 11:25:53 -0400

    made card

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+    <h2>Lewis Hamilton</h2>
+    <img src="https://a.espncdn.com/i/headshots/rpm/players/full/868.png" alt="Image Description" />
+    <input type="checkbox" id="details-toggle" className="details-toggle" />
+    <div className="description">
+      <label htmlFor="details-toggle" className="details-label">
+        Details
+      </label>
+      <p>
+        Lewis Hamilton is a British racing legend, renowned for his exceptional skills in Formula 1. With numerous world championships to his name, he's a trailblazer both on and off the track.
+      </p>
     </div>
+  </div>
+
   );
 }
 
